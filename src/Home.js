84094765fee755fec1ec6243ec2ed79022180784
Vishesh <visheshsingh16@gmail.com>
2018-12-08 23:31:22 -0500

    Making API call to fetch posts

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Home = () => {
-  return (
-    <div className="home">
-      <h1>Home</h1>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt nobis
-        error dolores natus vitae nam officiis velit nemo consectetur aspernatur
-        illo pariatur, fugit aut rerum?
-      </p>
-    </div>
-  );
-};
+class Home extends Component {
+  state = {
+    posts: []
+  };
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(res => res.json())
+      .then(posts => {
+        return this.setState({
+          posts: posts
+        });
+      });
+  }
+  render() {
+    const { posts } = this.state;
+    let postList = posts.length ? (
+      posts.map(post => {
+        return (
+          <div>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </div>
+        );
+      })
+    ) : (
+      <p>Loading posts...</p>
+    );
+    return (
+      <div className="home">
+        <h1>Home</h1>
+        <div>{postList}</div>
+      </div>
+    );
+  }
+}
 
 export default Home;
